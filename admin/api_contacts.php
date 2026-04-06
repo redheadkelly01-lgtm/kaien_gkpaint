bffb4606688ab83ce3a700c99571c78f8f2270a7
redheadkelly01-lgtm <redheadkelly01@gmail.com>
2026-04-06 11:57:58 +0900

    fix(admin): apply decryption layer to api_contacts.php

--- admin/api_contacts.php
+++ admin/api_contacts.php
@@ -2,14 +2,11 @@
 // .htaccessでアクセス拒否されているcontacts.jsonを、Basic認証済みのadmin側から読み込むための中継API
 header('Content-Type: application/json; charset=utf-8');
 
+require_once __DIR__ . '/../contact_sec.php';
+
 $file = __DIR__ . '/../data/contacts.php';
 $oldFile = __DIR__ . '/../data/contacts.json';
 
-if (file_exists($file)) {
-    $raw = file_get_contents($file);
-    echo preg_replace('/^<\?php exit; \?>\s*/', '', $raw);
-} elseif (file_exists($oldFile)) {
-    readfile($oldFile);
-} else {
-    echo '[]';
-}
+$contacts = read_contacts_data($file, $oldFile);
+echo json_encode($contacts, JSON_UNESCAPED_UNICODE);
+
